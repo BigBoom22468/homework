4d16758e7133956b191dfc21bd854ba90fcfebf3
BigBoom22468 <toniindeath@mail.ru>
2016-10-03 00:47:19 +0400

    Add files via upload

---
+++ vivod.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+//program < text.txt >> text.txt
+int main() {
+    FILE *f;
+    char s[255];
+    int line, word, flag, i;
+    line = 0;
+    word = 0;
+    f = fopen("text.txt", "r");
+    while (fgets (s, 255, f) != NULL) {
+        line += 1;
+        flag = 0;
+        for (i=0; i < strlen(s); i++) {
+            if (s[i] != ' ' && flag == 0) {
+                word += 1;
+                flag = 1;
+            }
+            else
+                if (s[i] == ' ') {
+                    flag = 0;
+            }
+        }
+    }
+    printf("%d lines\n%d words\n", line, word);
+    fclose(f);
+}
