a9259528c57602b2944c44917b673362835ab982
BigBoom22468 <toniindeath@mail.ru>
2016-10-08 17:58:36 +0300

    Add files via upload

---
+++ Twoprocess.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(){
+	pid_t pid = fork();
+	int PID;
+	int status = 1;
+	
+	if (pid < 0) {
+		perror("fork!");
+		exit(-1);
+	}
+	
+	if (pid > 0) {
+		//parent
+		while(pid > 0) {
+			while(1) {
+				waitpid(pid, &status, 0);
+				if(status == 0) {
+					pid = fork();
+					break;
+				}
+				status = 0;
+			}
+		}	
+	}
+	if (pid == 0) {
+		while(getppid() != 1000);
+		execl("temp", "temp", NULL);
+	}
+	return 0;
+}
