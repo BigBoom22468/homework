1cb4c11b443577f7db8b6f5dcd6dbce6da2294b6
BigBoom22468 <toniindeath@mail.ru>
2016-10-14 22:16:05 +0400

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <malloc.h>
 
 int main() {
-    int matrix[20][20], i, j, m, n;
+    int **matrix;
+    int i, j, m, n;
     scanf ("%d %d", &n, &m);
+    matrix = calloc(n, sizeof(int));
+    for(i = 0; i < n; i++) {
+	    matrix[i] = calloc(m, sizeof(int));
+    }
     printf("\n");
     for (i = 0; i < n; i++) {
         for (j = 0; j < m; j++){
